9cc9e343a0b2df8f7ec8c295dc959418532a0ce8
Demivdm <demi.van.der.maarl@hva.nl>
2023-11-01 15:58:49 +0100

    set up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Werkvormen from "./werkvormen/werkvormen.svelte";
+    import {posts} from "./werkvormen/werkvormen.svelte";
+   
+   
+</script>
+<h1>platform grote themas</h1>
+<Werkvormen {posts}/>
+
---
+++ src/routes/werkvormen/werkvormen.svelte
@@ -0,0 +1,68 @@
+<script context="module">
+  
+    // voor de const zou ik een apart bestand aan kunnen maken wanneer het teveel queries worden.
+   
+    // er wordt een verzoek om te data te laden verstuurd. dit wordt gedefinieerd in de functie.
+    // De load functie ontvangt fetch, hierbij wordt gebruik gemaakt van de fetch api
+    export async function load({ fetch }) {
+    const query = `query getpost {
+  posts {
+    nodes {
+      title
+    }
+  }
+}`;
+      const response = await fetch(
+        // er wordt een request gedaan naar het env bestand met een variabel met de url daarin
+        import.meta.env.VITE_PUBLIC_WORDPRESS_API_URL,
+        {
+          // het type verzoek is posten omdat de data opgehaald en gepost wordt op de pagina
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          // de query (het data verzoek) wordt opgezet in json?
+          body: JSON.stringify({ query }),
+        }
+      );
+      if (response.ok) {
+        const responseObj = await response.json();
+        const posts = responseObj.data.posts.nodes;
+        
+
+  
+        return {
+          props: { posts },
+          
+        };
+      }
+     
+    }
+  
+   
+    export let posts;
+    
+  </script>
+  <script>
+    export let posts
+   
+  </script>
+  
+  <h1>Werkvormen</h1>
+ 
+  
+ 
+  {#if posts}
+    <ul>
+      {#each posts as post}
+        <li>
+          <!-- <werkvorm {post} /> -->
+          <p>{post.title}</p>
+          
+        </li>
+      {/each}
+    </ul>
+  {:else}
+    <p>Oeps er zijn geen werkvormen beschikbaar</p>
+  {/if}
+  
